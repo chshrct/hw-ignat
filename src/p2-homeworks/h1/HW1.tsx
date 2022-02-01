128d377e4c01eb303d84f965278a3433ff1bdaca
Yauheni <eugene.kostroma@gmail.com>
2022-02-01 22:37:30 +0400

    hw-task1

---
+++ src/p2-homeworks/h1/HW1.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import Message from "./Message";
+
+const messageData = {
+    avatar: 'https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg',
+    name: 'Some Name',
+    message: 'some text',
+    time: '22:00',
+}
+const messageData2 = {
+    avatar: 'https://i.pinimg.com/originals/85/dd/71/85dd71c7c80d54583d6c2d078342637a.jpg',
+    name: 'Bender',
+    message: 'Bite my shiny metal ass!',
+    time: '22:10',
+}
+
+function HW1() {
+    return (
+        <div>
+            <hr/>
+            homeworks 1
+
+            should work (должно работать)
+
+            <Message
+                avatar={messageData.avatar}
+                name={messageData.name}
+                message={messageData.message}
+                time={messageData.time}
+            />
+            <Message
+                avatar={messageData2.avatar}
+                name={messageData2.name}
+                message={messageData2.message}
+                time={messageData2.time}
+            />
+            <hr/>
+            {/*для личного творчества, могу проверить*/}
+            {/*<AlternativeMessage/>*/}
+            <hr/>
+        </div>
+    )
+}
+
+export default HW1
---
+++ src/p2-homeworks/h1/Message.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import cl from './Message.module.css'
+
+
+type msgPropsType ={
+    avatar:string
+    name:string
+    message: string
+    time: string
+}
+
+
+
+
+function Message(props:msgPropsType){
+    return (
+        <div className={cl.wrapper}>
+            <div className={cl.avatar_wrapper}>
+                <img src={props.avatar} alt="avatar"/>
+            </div>
+            <div className={cl.message_wrapper}>
+                <div className={cl.author}>{props.name}</div>
+                <div className={cl.message}>{props.message}</div>
+                <div className={cl.time}>{props.time}</div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Message
