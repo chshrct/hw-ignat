ad76c9969a36736cb93a8824d25348b8d770f7a6
Yauheni <eugene.kostroma@gmail.com>
2022-02-15 18:01:03 +0400

    hw-task3

--- src/p2-homeworks/h3/GreetingContainer.tsx
+++ src/p2-homeworks/h3/GreetingContainer.tsx
@@ -1,9 +1,10 @@
-import React, {useState} from 'react'
+import React, {ChangeEvent,KeyboardEvent, useState} from 'react'
 import Greeting from './Greeting'
+import {UserType} from "./HW3";
 
 type GreetingContainerPropsType = {
-    users: any // need to fix any
-    addUserCallback: any // need to fix any
+    users: Array<UserType>
+    addUserCallback: (name:string)=>void
 }
 
 // более простой и понятный для новичков
@@ -12,17 +13,40 @@ type GreetingContainerPropsType = {
 // более современный и удобный для про :)
 // уровень локальной логики
 const GreetingContainer: React.FC<GreetingContainerPropsType> = ({users, addUserCallback}) => { // деструктуризация пропсов
-    const [name, setName] = useState<any>('') // need to fix any
-    const [error, setError] = useState<any>('') // need to fix any
+    const [name, setName] = useState<string>('')
+    const [error, setError] = useState<string>('')
 
-    const setNameCallback = (e: any) => { // need to fix any
-        setName('') // need to fix
+    const setNameCallback = (e: ChangeEvent<HTMLInputElement> ) => {
+        const title = e.currentTarget.value
+        const wordRegExp = /^[a-zA-Zа-яА-Я]+$/
+        if(title===''){
+            setError('')
+            setName('')
+        }else
+        if (wordRegExp.test(title)){
+            setError('')
+            setName(title)
+        }
+        else{
+            setError('Имя должно состоять из букв')
+            setName('')
+        }
+
     }
     const addUser = () => {
-        alert(`Hello  !`) // need to fix
+        alert(`Hello  ${name}!`)
+        addUserCallback(name)
+        setName('')
     }
+    const isButtonDisabled = !(error==='' && name!=='')
 
-    const totalUsers = 0 // need to fix
+    const totalUsers = users.length
+
+    const addUserEnter = (e:KeyboardEvent<HTMLInputElement>)=>{
+        if(e.charCode===13 && error==='' && name!==''){
+            addUser()
+        }
+    }
 
     return (
         <Greeting
@@ -31,6 +55,8 @@ const GreetingContainer: React.FC<GreetingContainerPropsType> = ({users, addUser
             addUser={addUser}
             error={error}
             totalUsers={totalUsers}
+            isButtonDisabled={isButtonDisabled}
+            addUserEnter={addUserEnter}
         />
     )
 }
