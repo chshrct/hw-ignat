d001a01bea2ce33f20a6a6ee16510bb504106c0e
Yauheni <eugene.kostroma@gmail.com>
2022-03-05 18:44:13 +0400

    hw-task5-navigate-add

--- src/p2-homeworks/h5/Routes.tsx
+++ src/p2-homeworks/h5/Routes.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Route, Routes} from 'react-router-dom'
+import {Route, Routes,Navigate} from 'react-router-dom'
 import PreJunior from "./pages/PreJunior";
 import Junior from "./pages/Junior";
 import JuniorPlus from "./pages/JuniorPlus";
@@ -20,7 +20,7 @@ function Routing() {
                 {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу PRE_JUNIOR*/}
                 {/*exact нужен чтоб указать полное совподение (что после '/' ничего не будет)*/}
                 <Route path={'*'} element={<Error404/>}/>
-                <Route path={'/'} element={<PreJunior/>}/>
+                <Route path={'/'} element={<Navigate to={PATH.PRE_JUNIOR}/>}/>
                 <Route path={PATH.PRE_JUNIOR} element={<PreJunior/>}/>
                 <Route path={PATH.JUNIOR} element={<Junior/>}/>
                 <Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus/>}/>
