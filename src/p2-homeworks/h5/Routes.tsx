a7d212cd0b19108d597c35a4060fc28e3ea3a314
Yauheni <eugene.kostroma@gmail.com>
2022-03-11 14:55:36 +0400

    hw-task5-fixed-error-route

--- src/p2-homeworks/h5/Routes.tsx
+++ src/p2-homeworks/h5/Routes.tsx
@@ -19,14 +19,14 @@ function Routing() {
 
                 {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу PRE_JUNIOR*/}
                 {/*exact нужен чтоб указать полное совподение (что после '/' ничего не будет)*/}
-                <Route path={'*'} element={<Error404/>}/>
+
                 <Route path={'/'} element={<Navigate to={PATH.PRE_JUNIOR}/>}/>
                 <Route path={PATH.PRE_JUNIOR} element={<PreJunior/>}/>
                 <Route path={PATH.JUNIOR} element={<Junior/>}/>
                 <Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus/>}/>
 
                 {/*у этого роута нет пути, он отрисуется если пользователь захочет попасть на несуществующую страницу*/}
-                {/*<Route render={() => <Error404/>}/>*/}
+                <Route path={'/*'} element={<Error404/>}/>
 
             </Routes>
         </div>
