2b36e1ad10501096f78571642c1f8827f820e570
Yauheni <eugene.kostroma@gmail.com>
2022-03-17 12:52:37 +0400

    hw-task7-2

--- src/p2-homeworks/h7/common/c6-SuperRadio/SuperRadio.tsx
+++ src/p2-homeworks/h7/common/c6-SuperRadio/SuperRadio.tsx
@@ -1,11 +1,9 @@
-import React, { ChangeEvent, InputHTMLAttributes, DetailedHTMLProps } from 'react'
+import React, {ChangeEvent, InputHTMLAttributes, DetailedHTMLProps} from 'react'
 import s from './SuperRadio.module.css'
 
 
-type DefaultRadioPropsType = DetailedHTMLProps<
-    InputHTMLAttributes<HTMLInputElement>,
-    HTMLInputElement
->
+type DefaultRadioPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>,
+    HTMLInputElement>
 
 type SuperRadioPropsType = DefaultRadioPropsType & {
     options?: any[]
@@ -13,43 +11,44 @@ type SuperRadioPropsType = DefaultRadioPropsType & {
 }
 
 const SuperRadio: React.FC<SuperRadioPropsType> = ({
-    type,
-    name,
-    options,
-    value,
-    onChange,
-    onChangeOption,
-    className,
-    ...restProps
-}) => {
+                                                       type,
+                                                       name,
+                                                       options,
+                                                       value,
+                                                       onChange,
+                                                       onChangeOption,
+                                                       className,
+                                                       ...restProps
+                                                   }) => {
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
         // onChange, onChangeOption
         onChange && onChange(e)
         onChangeOption && onChangeOption(e.currentTarget.value)
     }
 
-    const radioClassName = s.radio + ' ' + (className?className:'')
+    const radioClassName = s.radio + ' ' + (className ? className : '')
 
     const mappedOptions: any[] = options
         ? options.map(
-              (
-                  o,
-                  i // map options with key
-              ) => (
-                  <label key={name + '-' + i}>
-                      <input
-                          type={'radio'}
-                          name={name}
-                          onChange={onChangeCallback}
-                          value={o}
-                          checked={value === o}
-                          {...restProps}
-                          // name, checked, value, onChange
-                      />
-                      {o}
-                  </label>
-              )
-          )
+            (
+                o,
+                i // map options with key
+            ) => (
+                <label key={name + '-' + i}>
+                    <input
+                        type={'radio'}
+                        name={name}
+                        onChange={onChangeCallback}
+                        value={o}
+                        checked={value === o}
+                        className={radioClassName}
+                        {...restProps}
+                        // name, checked, value, onChange
+                    />
+                    {o}
+                </label>
+            )
+        )
         : []
 
     return <>{mappedOptions}</>
